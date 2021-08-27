a6feb12153d31e7814a3cec64522a00020cc1054
Carlos M Garcia <cgarcia101015@gmail.com>
2021-08-27 17:05:54 -0400

    charAndBoolean

---
+++ charAndBoolean/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char mychar = 'D';
+        char myUnicodeChar = '\u0044';
+
+
+        System.out.println(mychar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
