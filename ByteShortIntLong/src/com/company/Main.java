74aa441c31ec7f776901eb95981b088e74c414d4
Carlos M Garcia <cgarcia101015@gmail.com>
2021-08-27 14:00:25 -0400

    casting

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -33,6 +33,14 @@ public class Main {
         long bigLongLiteralValue = 2_147_483_647_243L;
         System.out.println(bigLongLiteralValue);
 
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
 
     }
 }
