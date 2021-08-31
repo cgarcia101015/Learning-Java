b68bacbc07318015661fa8b5baf801b437aa093f
Carlos M Garcia <cgarcia101015@gmail.com>
2021-08-30 23:47:04 -0400

    strings recap

---
+++ strings/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //byte
+        //int
+        //short
+        //long
+        //float
+        //double
+        //char
+        //boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.00";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+    }
+}
