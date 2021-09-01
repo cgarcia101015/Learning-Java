c6d8852a47394e581671ff9bb77f383e14c21e20
Carlos M Garcia <cgarcia101015@gmail.com>
2021-09-01 01:18:56 -0400

    ternary operator

--- operators/src/com/company/Main.java
+++ operators/src/com/company/Main.java
@@ -66,5 +66,21 @@ public class Main {
             System.out.println("Either or both of the conditions are true");
         }
 
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
     }
 }
