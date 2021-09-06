00bde3975fa299858e0c329d02db27bdab200a11
Carlos M Garcia <cgarcia101015@gmail.com>
2021-09-06 01:54:27 -0400

    methods.v1

--- ifKeywordsAndCodeblocks/src/com/company/Main.java
+++ ifKeywordsAndCodeblocks/src/com/company/Main.java
@@ -3,30 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    boolean gameOver = true;
-	    int score = 800;
-	    int levelCompleted = 5;
-	    int bonus = 100;
 
-//	    if (score < 5000 && score > 1000) {
-//            System.out.println("Your score was 5000 but greater than 1000");
-//        } else if (score < 1000){
-//			System.out.println("Your score was less than 1000");
-//		} else {
-//			System.out.println("Got here");
-//		}
+    	boolean gameOver = true;
+    	int score = 800;
+    	int levelCompleted = 5;
+    	int bonus = 100;
+
+	    calculateScore(gameOver, score, levelCompleted, bonus);
+
+	    score = 10000;
+	    levelCompleted = 8;
+	    bonus = 200;
+
+		calculateScore(gameOver, score, levelCompleted, bonus);
+		
+
+
+
+	}
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
 		if (gameOver) {
 			int finalScore = score + (levelCompleted * bonus);
+			finalScore += 2000;
 			System.out.println("Your final score was " + finalScore);
+			return finalScore;
 		}
-		boolean newGameOver = true;
-		int newScore = 10000;
-		int newLevelCompleted = 8;
-		int newBonus = 200;
-
-		if (gameOver) {
-			int newFinalScore = newScore + (newLevelCompleted * newBonus);
-			System.out.println("Your second score is " + newFinalScore);
-		}
-    }
+			return -1;
+	}
 }
