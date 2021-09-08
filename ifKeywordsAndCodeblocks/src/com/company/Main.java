77fe439496d0c2f92202a605b3f0cb8e312ec164
Carlos M Garcia <cgarcia101015@gmail.com>
2021-09-08 00:29:32 -0400

    methods.v2

--- ifKeywordsAndCodeblocks/src/com/company/Main.java
+++ ifKeywordsAndCodeblocks/src/com/company/Main.java
@@ -9,27 +9,64 @@ public class Main {
     	int levelCompleted = 5;
     	int bonus = 100;
 
-	    calculateScore(gameOver, score, levelCompleted, bonus);
+    	int scorePosition;
 
-	    score = 10000;
-	    levelCompleted = 8;
-	    bonus = 200;
+//	   	int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//		System.out.println("Your final score was " + highScore);
+//
+//	    score = 10000;
+//	    levelCompleted = 8;
+//	    bonus = 200;
+//
+//		highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+//		System.out.println("Your final score was " + highScore);
 
-		calculateScore(gameOver, score, levelCompleted, bonus);
-		
+		scorePosition = calculateHighScorePosition(1500);
+		displayHighScorePosition("Carlos", scorePosition);
+
+		scorePosition = calculateHighScorePosition(900);
+		displayHighScorePosition("Tom", scorePosition);
+
+		scorePosition = calculateHighScorePosition(400);
+		displayHighScorePosition("Jane", scorePosition);
+
+		scorePosition = calculateHighScorePosition(50);
+		displayHighScorePosition("Bill", scorePosition);
 
 
 
 	}
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//
+//		if (gameOver) {
+//			int finalScore = score + (levelCompleted * bonus);
+//			finalScore += 2000;
+//			return finalScore;
+//		}
+//			return -1;
+//	}
 
-		if (gameOver) {
-			int finalScore = score + (levelCompleted * bonus);
-			finalScore += 2000;
-			System.out.println("Your final score was " + finalScore);
-			return finalScore;
+
+	public static void displayHighScorePosition(String playerName, int scoreLevel){
+		System.out.println(playerName + " managed to get into position " + scoreLevel + " on the high score table.");
+
+	}
+
+	public static int calculateHighScorePosition(int playerScore){
+
+    	if (playerScore >= 1000) {
+			return 1;
+		} else if (playerScore >= 500 && playerScore < 1000) {
+			return 2;
+		} else if (playerScore >= 100 && playerScore < 500) {
+			return 3;
+		} else {
+			return 4;
 		}
-			return -1;
+
 	}
+
+
+
 }
