a9dc5a37cdb9a5ca7f3ab53bde9f73c5e5206bfd
Carlos M Garcia <cgarcia101015@gmail.com>
2021-09-13 00:40:12 -0400

    speedConverter

---
+++ speedConverter/src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+
+    public static long twoMilesPerHour(double kilometersPerHour){
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion (double kilometersPerHour) {
+        if(kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = twoMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
